3db184d9953ef2946db2d9077ca9ad2eb91830e0
niteeshpatil <niteeshgpatil5@gmail.com>
2024-11-01 15:49:59 +0530

    setup of base project for Taskmate with Spring Initializr

---
+++ src/test/java/com/TaskMate/TaskMate/TaskMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TaskMate.TaskMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
